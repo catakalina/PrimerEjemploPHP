db0e30a0c77e093b050c2c9e293eeef81ddc65d0
catakalina <79998936+catakalina@users.noreply.github.com>
2021-03-18 10:01:45 -0500

    contenido segunda clase

--- index.php
+++ index.php
@@ -1,37 +1,70 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form action="index.php" method="get">
-    <input type="text" name="name" id="name">
-    <input type="submit" value="Enviar">
-</form>
-</body>
-</html>
-
 <?php
-$nombre = $_GET['name'] ?? '';
+$nombre = "catalina";
+var_dump($nombre);
+$nombre = 10;
+var_dump($nombre);
+$nombre = TRUE; //True, true
+$nombre = FALSE; //False, false
+var_dump($nombre);
+$nombre = "20"."10";
+var_dump($nombre);
 
-echo 'Hola $nombre </br>';//Comillas Simples
-echo "Hola ".$nombre;   //Comillas Dobles
 
-if ($nombre == "catalina"){
+$nombre = null; //0, -0 => FALSE, 1 => TRUE
+var_dump((bool) $nombre);
+
+$numero = 1.2e3;
+var_dump($numero);
+
+$nombre = "Catalina";
+
+$arrayUsuarios = [
+    [
+        "Nombre" => "Catalina Rodriguez",
+        "Edad" => 19,
+        "Genero" => "Femenino",
+        "Peso" => 50.0,
+        "Administrador" => false
+    ],
+    [
+        "Nombre" => "Laura Rodriguez",
+        "Edad" => 17,
+        "Genero" => "Femenino",
+        "Peso" => 60.0,
+        "Administrador" => true
+    ],
+];
+
+var_dump($arrayUsuarios);
+
+foreach ($arrayUsuarios as $usuario){
+    echo $usuario['Nombre']."</br>";
+}
+
+// Corto de una sola linea
+/*
+ * Comentario de varias lineas
+ */
+# Tambien sirve para comentar una linea
+//echo 'Hola $nombre </br>';//Comillas Simples
+//echo "Hola ".$nombre;   //Comillas Dobles
+/*if ($nombre == "catalina"){
     echo "Hola ".$nombre;
 }else if($nombre == "lau"){
     echo "Hola lau";
 }else{
     echo "Hola Usuario";
-}
+}*/
 ?>
 
-<?php if ($nombre == "catalina"){ ?>
-    Esto se mostrará si la expresión es verdadera.
-<?php }else{ ?>
-    En caso contrario se mostrará esto.
-<?php }?>
+
+<?php
+
+class Usuario {
+
+    // boolean, integer, float, string.
+    // array, object, callable, iterable
+    // resource y null
+    private string $nombre = "catalina";
+
+}
